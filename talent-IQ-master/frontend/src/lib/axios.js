a2b0fe9ee4873790316550cc658c48a1c45007a7
vedabhilash <vedabhilash955@gmail.com>
2026-01-09 17:15:04 +0530

    Initial commit

--- talent-IQ-master/frontend/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
-});
-
-export default axiosInstance;
